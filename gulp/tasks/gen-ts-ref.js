95441bed30f72f0e12a6da9dd530ec3782f5f214
Bob Monteverde <bob@mashwork.com>
2015-08-07 09:39:20 -0400

    added gen-ts-ref gulp task

---
+++ gulp/tasks/gen-ts-ref.js
@@ -0,0 +1,26 @@
+'use strict';
+
+import gulp from 'gulp';
+import browserSync from 'browser-sync';
+import config from '../config';
+import utils from '../utils';
+import gulpLoadPlugins from 'gulp-load-plugins';
+
+const $ = gulpLoadPlugins();
+
+$.help(gulp); // provide help through 'gulp help' -- the help text is the second gulp task argument (https://www.npmjs.com/package/gulp-help/)
+
+
+// Generates the app.d.ts references file dynamically from all application *.ts files.
+gulp.task('gen-ts-refs', function () {
+    var target = gulp.src(config.files.appTypeScriptReferences);
+    var sources = gulp.src(config.typescript.srcAppOnly, {read: false});
+    return target.pipe($.inject(sources, {
+        starttag: '//{',
+        endtag: '//}',
+        transform: function (filepath) {
+            return '/// <reference path="../..' + filepath + '" />';
+        }
+    })).pipe(gulp.dest(config.folders.typings));
+});
+
